84966dcf9213053e3a8496d5e772d0fcab5cfa25
David Goodwin <david@zx.net.nz>
2013-10-26 21:51:16 +1300

    Update build scripts
    mkdist now moves manifest files into dist directory, mkntd now works
    without tee and will automatically create the ntd subdirectory if it
    doesn't exist

--- kermit/k95/mkdist.bat
+++ kermit/k95/mkdist.bat
@@ -1,5 +1,7 @@
 @echo Making distribution
 if not exist dist\NUL mkdir dist
 move *.exe dist
+move *.manifest dist
 copy k95.ini dist
 ren dist\cknker.exe k95.exe
+ren dist\cknker.exe.manifest k95.exe.manifest
--- kermit/k95/mkntd.bat
+++ kermit/k95/mkntd.bat
@@ -1,7 +1,8 @@
 SET PLATFORM=NT
 SET K95BUILD=K95
+if not exist ntd\NUL mkdir ntd
 move ntd\*.obj . > nul
 del ckcmai.obj ckuus5.obj
-nmake /nologo /e /f ckoker.mak msvcd | tee comp.out.nt | list /s
+nmake /nologo /e /f ckoker.mak msvcd
 move *.obj ntd > nul 
 
