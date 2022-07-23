b218c7d00b36a57e810a4ddfa6ce8cabfc6e5d64
David Goodwin <david@zx.net.nz>
2022-07-23 14:09:58 +1200

    Tidy up setenv.bat

--- setenv.bat
+++ setenv.bat
@@ -1,4 +1,25 @@
+@echo off
+
+REM Set this to where your source code lives
 set root=C:\src
 
-set include=%include%;%root%\kermit\k95;%root%\openssl\0.9.7\inc32;%root%\srp\include;%root%\pwsdk\inc32;%root%\kerberos\kfw-2.2-beta-2\athena\wshelper\include;%root%\superlat\include;%root%\kerberos\kfw-2.2-beta-2\athena\auth\krb5\src\include;%root%\kermit\k95\kui;%root%\zinc\include;
-set lib=%lib%;%root%\kerberos\kfw-2.2-beta-2\target\lib\i386\rel;%root%\zinc\lib\mvcpp500
+set include=%include%;%root%\kermit\k95
+set include=%include%;%root%\kermit\k95\kui
+
+REM Include paths for obsolete libraries we're not currently doing
+REM anything with.
+REM set include=%include%;%root%\openssl\0.9.7\inc32
+REM set include=%include%;%root%\srp\include
+REM set include=%include%;%root%\pwsdk\inc32
+REM set include=%include%;%root%\kerberos\kfw-2.2-beta-2\athena\wshelper\include
+REM set include=%include%;%root%\superlat\include
+REM set include=%include%;%root%\kerberos\kfw-2.2-beta-2\athena\auth\krb5\src\include
+REM set include=%include%;%root%\zinc\include
+
+REM Library paths for obsolete libraries we're not currently doing
+REM anything with.
+REM set lib=%lib%;%root%\kerberos\kfw-2.2-beta-2\target\lib\i386\rel
+REM set lib=%lib%;%root%\zinc\lib\mvcpp500
+
+echo Include path set to: %include%
+echo Library path set to: %lib%
