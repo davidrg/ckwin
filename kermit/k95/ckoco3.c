b74faaadd3499e167d856ad50056d7bc976b277b
David Goodwin <david@zx.net.nz>
2022-07-25 07:52:25 +1200

    Fix definition of popen and pclose
    Watcom C provides _popen and _pclose. We get link errors without this:
    Error! E2028: popen_ is an undefined reference
    Error! E2028: pclose_ is an undefined reference

--- kermit/k95/ckoco3.c
+++ kermit/k95/ckoco3.c
@@ -125,6 +125,11 @@ _PROTOTYP( FILE * win95popen, (const char *cmd, const char *mode) );
 _PROTOTYP( int win95pclose, (FILE *pipe) );
 #define popen _popen
 #define pclose _pclose
+#else
+#ifdef __WATCOMC__
+#define popen _popen
+#define pclose _pclose
+#endif
 #endif /* NT */
 
 #ifdef COMMENT
