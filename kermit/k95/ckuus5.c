62ee3a6e4c4ca28903a9bb68dad446f80bb8f987
David Goodwin <david@zx.net.nz>
2022-08-01 22:13:40 +1200

    Get rid of ConPTY from show features
    Now that its enabled via NETPTY it already shows

--- kermit/k95/ckuus5.c
+++ kermit/k95/ckuus5.c
@@ -12204,11 +12204,6 @@ shofea() {
     if (++lines > cmd_rows - 3) { if (!askmore()) return(1); else lines = 0; }
 #endif /* IKSD */
 
-#ifdef CK_CONPTY
-    printf(" Windows PTY support (Windows 10+)\n");
-    if (++lines > cmd_rows - 3) { if (!askmore()) return(1); else lines = 0; }
-#endif /* CK_CONPTY */
-
     printf("\n");
     if (++lines > cmd_rows - 3) { if (!askmore()) return(1); else lines = 0; }
     printf("Major optional features not included:\n");
@@ -12578,11 +12573,6 @@ shofea() {
     if (++lines > cmd_rows - 3) { if (!askmore()) return(1); else lines = 0; }
     flag = 1;
 #endif /* IKSD */
-#ifdef CK_CONPTY
-    printf(" No Windows PTY support (Windows 10+)\n");
-    if (++lines > cmd_rows - 3) { if (!askmore()) return(1); else lines = 0; }
-    flag = 1;
-#endif /* CK_CONPTY */
 
     if (flag == 0) {
         printf(" None\n");
