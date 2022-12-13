f5aed83f9fbca2c16169ca826da958666c7230e1
David Goodwin <david@zx.net.nz>
2022-12-13 21:58:06 +1300

    Rename dialer license file to match others

--- kermit/dialer/mkdist.bat
+++ kermit/dialer/mkdist.bat
@@ -9,7 +9,7 @@ copy *.manifest dist
 copy dialer.dat dist
 copy p_servic.znc dist
 copy p_direct.znc dist
-copy %root%\zinc\COPYING.LESSOR.txt dist
+copy %root%\zinc\COPYING.LESSOR.txt dist\COPYING.dialer.txt
 copy registry\k95regtl.txt dist
 copy registry\k95regtl.exe dist
 REM copy registry\k95regtl.exe.manifest dist
