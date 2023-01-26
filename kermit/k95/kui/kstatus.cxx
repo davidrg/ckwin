8bc7be9dcbf1ee9910ac501f451dd4219f93feb5
David Goodwin <david@zx.net.nz>
2023-01-27 12:53:19 +1300

    Fix status bar on NT 3.10
    As well as for NT 3.10+3.50. No code change for builds targeting NT 3.50+
    
    This fixes the crazy resize behaviour of Visual C++ 2.0 builds on NT 3.10

--- kermit/k95/kui/kstatus.cxx
+++ kermit/k95/kui/kstatus.cxx
@@ -73,7 +73,54 @@ KStatus::~KStatus()
 ------------------------------------------------------------------------*/
 void KStatus::getCreateInfo( K_CREATEINFO* info )
 {
+    // It took me several hours to figure out why window sizing was breaking
+    // on NT 3.1 when built with Visual C++ 2.0, then why the status bar
+    // dimensions were coming back as absurd values on NT 3.1 only, then why
+    // the status bar wasn't rendering at all.
+    //  Visual C++ 1.0: STATUSCLASSNAME "msctls_statusbar"
+    //  Visual C++ 2.0: STATUSCLASSNAME "msctls_statusbar32"
+    // Even worse, NT 3.50 is not backwards compatible with NT 3.1. The class
+    // name "msctls_statusbar" only works on NT 3.10
+
+
+#ifdef CKT_NT31ONLY
+    info->classname = "msctls_statusbar";
+#else
+#ifdef CKT_NT35_AND_31
+    // Detect which we're running on and use the appropriate class name
+    DWORD dwVersion;
+    int major, minor, build;
+
+    dwVersion = GetVersion();
+
+    major = LOBYTE(LOWORD(dwVersion));
+    minor = HIBYTE(LOWORD(dwVersion));
+    build = HIWORD(dwVersion);
+
+    if (dwVersion < 0x80000000) {
+        /* Windows NT */
+        if (major == 3 && minor < 50) {
+            /* Older than NT 3.50, use NT 3.10 class name
+             * (ideally we'd check the build number too so the oldest
+             * NT 3.50 betas work properly) */
+            info->classname = "msctls_statusbar";
+        } else {
+            /* NT 3.50 or newer - use standard class name */
+            info->classname = STATUSCLASSNAME;
+        }
+    } else if (LOBYTE(LOWORD(dwVersion))<4) {
+        /* Win32s - use the same as NT 3.10??? */
+        info->classname = "msctls_statusbar";
+    } else {
+        /* Windows 95 - use standard class name*/
+        info->classname = STATUSCLASSNAME;
+    }
+#else
+    // NT 3.1 not supported at all, use standard class name
     info->classname = STATUSCLASSNAME;
+#endif /* CKT_NT35_AND_31 */
+#endif /* CKT_NT31ONLY */
+    
     info->objId = ::GetGlobalID();
     info->style = WS_CHILD | WS_CLIPSIBLINGS | WS_VISIBLE | SBT_NOBORDERS
 #ifndef CKT_NT35_OR_31
