fb99c72841cd40e6108da53252da498f78c10d7e
David Goodwin <david@zx.net.nz>
2023-02-04 12:19:34 +1300

    cko32i20.dll (and other network DLLS) should be distributed

--- kermit/k95/mkdist-os2.bat
+++ kermit/k95/mkdist-os2.bat
@@ -8,6 +8,7 @@ if not exist dist-os2\docs\manual\NUL mkdir dist-os2\docs\manual
 
 @echo Move build outputs...
 move *.exe dist-os2
+move cko32*.dll dist-os2
 move dist-os2\ckwart.exe .\
 move dist-os2\osetup.exe .\
 ren dist-os2\ckoker32.exe k2.exe
