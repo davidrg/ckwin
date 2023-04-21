b136c5e55a0fc7bd0e4bf9b3993d09c20451daad
David Goodwin <david@zx.net.nz>
2023-04-21 20:42:12 +1200

    Fix setenv issues

--- setenv.bat
+++ setenv.bat
@@ -62,7 +62,6 @@ REM ============================================================================
 
 REM Figure out if we're doing a 64bit build or a 32bit one (this affects our
 REM choices for some libraries later on)
-set CKB_BITS_64=no
 if exist %WATCOM%\binnt\wcl386.exe goto :bitcheckdone
 cl 2>&1 | findstr /C:"for x64" > nul
 if %errorlevel% == 0 goto :bits64
@@ -78,9 +77,15 @@ REM Yes, the 64bit Windows for Alpha compiler exist. No, you can't run its outpu
 REM on anything (unless you happen to work for Microsoft)
 cl 2>&1 | findstr /R /C:"Digital.*Alpha.*Version 13.0" > nul
 if %errorlevel% == 0 goto :bits64
+
+set CKB_BITS_64=no
+echo Targeting 32bit Windows
+goto :bitcheckdone
+
 :bits64
 REM Targeting a 64bit host (x86-64, IA64, ARM64, AXP64)
 set CKB_BITS_64=yes
+echo Targeting 64bit Windows
 
 REM These libraries aren't supported for 64bit targets - force them off
 set CKF_SRP=no
@@ -95,7 +100,7 @@ set ckinclude=%root%\kermit\k95
 
 REM See if the user has the SuperLAT SDK (*extremely* unlikely)
 if "%CKF_SUPERLAT%" == "no" echo Skipping check for SuperLAT
-if "%CKF_SUPERLAT%" == "no" goto :novcpkgzlib
+if "%CKF_SUPERLAT%" == "no" goto :nosuperlat
 set CKF_SUPERLAT=no
 if exist %root%\superlat\include\latioc.h set CKF_SUPERLAT=yes
 if exist %root%\superlat\include\latioc.h set ckinclude=%ckinclude%;%root%\superlat\include
@@ -149,18 +154,19 @@ if exist %vcpkg_installed%\bin\zlib1.dll set CK_ZLIB_DIST_DLLS=%vcpkg_installed%
 if "%CKF_SSL%" == "no" echo Skipping check for OpenSSL
 if "%CKF_SSL%" == "no" goto :novcpkgssl
 if exist %vcpkg_installed%\lib\libssl.lib set CKF_SSL=yes
-if exist %vcpkg_installed%\bin\libcrypto-1_1.dll set CK_SSL_DIST_DLLS=%CK_SSL_DIST_DLLS% %openssl_root%\libcrypto-1_1.dll %openssl_root%\libssl-1_1.dll
 if exist %vcpkg_installed%\tools\openssl\openssl.exe set CK_SSL_DIST_DLLS=%CK_SSL_DIST_DLLS% %openssl_root%\tools\openssl\openssl.exe
 
 REM The OpenSSL3 DLL names vary based on arch
 if "%CKB_BITS_64%" == "yes" goto :vcpkossl64bit
 REM else 32bit
 
+if exist %vcpkg_installed%\bin\libcrypto-1_1.dll set CK_SSL_DIST_DLLS=%CK_SSL_DIST_DLLS% %openssl_root%\libcrypto-1_1.dll %openssl_root%\libssl-1_1.dll
 if exist %vcpkg_installed%\bin\libcrypto-3.dll set CK_SSL_DIST_DLLS=%vcpkg_installed%\bin\libcrypto-3.dll %vcpkg_installed%\bin\libssl-3.dll
 
 goto :novcpkgssl
 :vcpkossl64bit
 
+if exist %vcpkg_installed%\bin\libcrypto-1_1-x64.dll set CK_SSL_DIST_DLLS=%CK_SSL_DIST_DLLS% %openssl_root%\libcrypto-1_1-x64.dll %openssl_root%\libssl-1_1-x64.dll
 if exist %vcpkg_installed%\bin\libcrypto-3-x64.dll set CK_SSL_DIST_DLLS=%vcpkg_installed%\bin\libcrypto-3-x64.dll %vcpkg_installed%\bin\libssl-3-x64.dll
 
 :novcpkgssl
@@ -204,9 +210,6 @@ if exist %openssl_root%\libssl.lib echo Found OpenSSL 1.1.x or 3.0.x
 if exist %openssl_root%\libssl.lib set CKF_SSL_LIBS=libssl.lib libcrypto.lib
 if exist %openssl_root%\apps\openssl.exe set CK_SSL_DIST_DLLS=%CK_SSL_DIST_DLLS% %openssl_root%\apps\openssl.exe
 
-REM OpenSSL 1.1.x
-if exist %openssl_root%\libcrypto-1_1.dll set CK_SSL_DIST_DLLS=%CK_SSL_DIST_DLLS% %openssl_root%\libcrypto-1_1.dll %openssl_root%\libssl-1_1.dll
-
 REM OpenSSL 0.9.8, 1.0.x:
 if exist %openssl_root%\out32dll\ssleay32.lib set lib=%lib%;%openssl_root%\out32dll
 if exist %openssl_root%\out32dll\ssleay32.lib set CKF_SSL=yes
@@ -219,11 +222,17 @@ REM OpenSSL libraries that vary based on 32bit/64bit
 if "%CKB_BITS_64%" == "yes" goto :ossl64bit
 REM else 32bit
 
+REM OpenSSL 1.1.x
+if exist %openssl_root%\libcrypto-1_1.dll set CK_SSL_DIST_DLLS=%CK_SSL_DIST_DLLS% %openssl_root%\libcrypto-1_1.dll %openssl_root%\libssl-1_1.dll
+
 REM OpenSSL 3.0.x
 if exist %openssl_root%\libcrypto-3.dll set CK_SSL_DIST_DLLS=%CK_SSL_DIST_DLLS% %openssl_root%\libcrypto-3.dll %openssl_root%\libssl-3.dll
 
 goto :nossl
 :ossl64bit
+
+REM OpenSSL 1.1.x
+if exist %openssl_root%\libcrypto-1_1-x64.dll set CK_SSL_DIST_DLLS=%CK_SSL_DIST_DLLS% %openssl_root%\libcrypto-1_1-x64.dll %openssl_root%\libssl-1_1-x64.dll
 
 REM OpenSSL 3.0.x
 if exist %openssl_root%\libcrypto-3-x64.dll set CK_SSL_DIST_DLLS=%CK_SSL_DIST_DLLS% %openssl_root%\libcrypto-3-x64.dll %openssl_root%\libssl-3-x64.dll
