54205bf52a969555983632580691e2e672e85c6f
David Goodwin <david@zx.net.nz>
2023-05-06 18:18:34 +1200

    Fix #197 - SSH v1 is not supported anymore (intro text)

--- kermit/k95/ckuus2.c
+++ kermit/k95/ckuus2.c
@@ -358,7 +358,7 @@ char *introtxt[] = {
 "   - Telnet sessions",
 #endif /* TNCODE */
 #ifdef SSHBUILTIN
-"   - SSH v1 and v2 connections",
+"   - SSH v2 connections",
 #else
 #ifdef ANYSSH
 "   - SSH connections via external agent",
