43eed9f1572977bfd7bc7b963cee76937a41d7d0
David Goodwin <david@zx.net.nz>
2026-06-01 16:07:33 +1200

    Report extension 7 (DRCS)

--- kermit/k95/ckoco3.c
+++ kermit/k95/ckoco3.c
@@ -575,7 +575,7 @@ int user_pages = -1; /* How many pages does the user want (SET TERM PAGE COUNT) 
   *  3 - ReGIS graphics
   *  4 - Sixel graphics
      6 - DECSED - Selective erase
-  *  7 - DRCS - Soft character sets
+     7 - DRCS - Soft character sets
      8 - UDK - User-defined keys
      9 - National Replacement Character Sets can be designated by host
   * 12 - Serbo-Croation (SCS)
@@ -642,9 +642,9 @@ struct tt_info_rec tt_info[] = {        /* Indexed by terminal type */
     "VT420", {"DEC-VT420","DEC-VT400","VT400",NULL},    "[?64;1;2;6;8;9;15;23;42;44;45;46c",       /* DEC VT420 */
     "VT525", {"DEC-VT525","DEC-VT500","VT500",NULL},    "[?65;1;2;6;8;9;15;22;23;42;44;45;46c",       /* DEC VT520 */
 #endif /* COMMENT */
-    "K95",    {"K95",NULL}, "[?63;1;2;6;8;9;15;22;28;32;44c",     /* Kermit 95 self-personality */
+    "K95",    {"K95",NULL}, "[?63;1;2;6;7;8;9;15;22;28;32;44c",     /* Kermit 95 self-personality */
             /* K95 Device Attributes:
-				VT320;132-columns;printer;selective-erase;user-defined-keys;
+				VT320;132-columns;printer;selective-erase;DRCS;user-defined-keys;
                 national-replacement-character-sets;technical-characters;
 				ansi-color;rectangular-editing;text-macros;PCTerm
                 TODO: do we *really* not have extension 42? ISO Latin-2 seems to be supported...
